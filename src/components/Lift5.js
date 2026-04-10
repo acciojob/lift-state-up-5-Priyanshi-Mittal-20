53cc65f52ac4a5546c8da97d1b194aef97f71ad4
Priyanshi Mittal <priyanshimzn05@gmail.com>
2026-04-10 11:57:37 +0530

    lift-5-2

--- src/components/Lift5.js
+++ src/components/Lift5.js
@@ -6,9 +6,9 @@ const Lift5=(props)=>{
   return(
     <div>
       <label htmlFor="username">Username:</label>
-      <input type="text" id="username" />
+      <input type="text" id="username" /><br/>
       <label htmlFor="password">Password</label>
-      <input type="password" id="password" />
+      <input type="password" id="password" /><br/>
       <button onClick={props.func}>Login</button>
     </div>
   )
