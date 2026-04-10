551b6b8ef7e4046ba711fc3be14c6b71dd41fea2
Priyanshi Mittal <priyanshimzn05@gmail.com>
2026-04-10 12:01:04 +0530

    lift-5-3

--- src/components/Lift5.js
+++ src/components/Lift5.js
@@ -5,11 +5,13 @@ import { useState } from "react";
 const Lift5=(props)=>{
   return(
     <div>
+      <form>
       <label htmlFor="username">Username:</label>
       <input type="text" id="username" /><br/>
       <label htmlFor="password">Password</label>
       <input type="password" id="password" /><br/>
       <button onClick={props.func}>Login</button>
+      </form>
     </div>
   )
 }
